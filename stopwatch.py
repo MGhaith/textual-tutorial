5d6ae3cd6d5701b558e72cb859aeac142eb66609
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-12-30 03:05:33 +0100

    add a header and a footer

--- stopwatch.py
+++ stopwatch.py
@@ -1,7 +1,10 @@
 from textual.app import App
+from textual.widgets import Footer, Header
 
 class StopwatchApp(App):
-    pass
+    def compose(self):
+        yield Header()
+        yield Footer()
 
 if __name__ == "__main__":
     StopwatchApp().run()
