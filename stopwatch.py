08dba9c4fe0d0cfa25003dc153a087025e4c3c42
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-12-30 03:19:34 +0100

    add keybinding for dark mode

--- stopwatch.py
+++ stopwatch.py
@@ -2,11 +2,18 @@ from textual.app import App
 from textual.widgets import Footer, Header
 
 class StopwatchApp(App):
+    # Bindings is a tuple: (key, action name, description), 
+    BINDINGS = [
+        ("d", "toggle_dark_mode", "Toggle dark mode"),
+    ]
+
     def compose(self):
         yield Header(show_clock=True)
         yield Footer()
 
-    def toggle_dark_mode(self):
+    # Action method (starts with: action_)
+    # Toggle dark mode
+    def action_toggle_dark_mode(self):
         self.dark = not self.dark
 
 if __name__ == "__main__":
