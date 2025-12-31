9544e6cc00335f59653f643130263723289ea157
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-12-31 03:08:09 +0100

    finished stop watch logic

--- stopwatch.py
+++ stopwatch.py
@@ -9,7 +9,15 @@ from textual.widgets import Footer, Header, Static, Button
 # Custom time display widget
 class TimeDisplay(Static):
     
+    accumelated_time = 0
+    start_time = monotonic()
     time_elapsed = reactive(0)
+
+    def _on_mount(self):
+        self.update_timer = self.set_interval(1/60, self.update_time_elapsed, pause=True)
+
+    def update_time_elapsed(self):
+        self.time_elapsed = self.accumelated_time + monotonic() - self.start_time
 
     def watch_time_elapsed(self):
         time = self.time_elapsed
@@ -21,14 +29,17 @@ class TimeDisplay(Static):
     # Start timer
     def start(self):
         self.start_time = monotonic()
+        self.update_timer.resume()
 
     # Stop timer
     def stop(self):
-        self.time_elapsed = monotonic() - self.start_time
+        self.accumelated_time = self.time_elapsed
+        self.update_timer.pause()
 
     # Reset timer
     def reset(self):
-        pass
+        self.accumelated_time = 0
+        self.time_elapsed = 0
 
 # Custom stopwatch widget
 class Stopwatch(Static):
