d2af8ba7f00b4e85bc21833484fda6a3d28ab68a
Ghaith Magherbi <retr0creativegm@gmail.com>
2026-01-01 22:57:27 +0100

    chnage the name from stopwatch app to clock app

--- clock/apps/clock_app.py
+++ clock/apps/clock_app.py
@@ -1,8 +1,49 @@
-from stopwatch_app import StopwatchApp
-from timer_app import TimerApp
+from widgets.stopwatch import Stopwatch
+from widgets.timer import Timer
 
 from textual.app import App
-
+from textual.containers import ScrollableContainer
+from textual.widgets import Footer, Header
 
 class ClockApp(App):
-    pass
+
+    CSS_PATH = "../styles/stopwatch.tcss"
+    BINDINGS = [
+        ("d", "toggle_dark_mode", "Toggle dark mode"),
+        ("a", "add_stopwatch", "Add a new stopwatch"),
+        ("r", "remove_stopwatch", "Remove stopwatch"),
+        ("q", "quit_app", "Quit Clock")
+    ]
+
+    def compose(self):
+        yield Header(show_clock=True)
+
+        with ScrollableContainer(id="stopwatches"):
+            yield Stopwatch()
+            yield Timer()
+
+        yield Footer()
+
+    ## Action methods (starts with: action_)
+    # Toggle dark mode
+    def action_toggle_dark_mode(self):
+        self.theme = ( 
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+         )
+        
+    # Add stopwatch
+    def action_add_stopwatch(self):
+        stopwatch = Stopwatch()
+        container = self.query_one("#stopwatches")
+        container.mount(stopwatch)
+        stopwatch.scroll_visible()
+
+    # Remove stopwatch
+    def action_remove_stopwatch(self):
+        stopwatches = self.query(Stopwatch)
+        if stopwatches:
+            stopwatches.last().remove()
+    
+    # Quit Clock
+    def action_quit_app(self):
+        self.exit() 
