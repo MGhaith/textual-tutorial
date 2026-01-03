bedd80e6fd9be13535d0e0a2f3c0ac7be40831f2
Ghaith Magherbi <retr0creativegm@gmail.com>
2026-01-03 12:20:20 +0100

    add tabs for different apps

--- clock/apps/clock_app.py
+++ clock/apps/clock_app.py
@@ -3,7 +3,12 @@ from widgets.timer import Timer
 
 from textual.app import App
 from textual.containers import ScrollableContainer
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, Tabs, Tab, ContentSwitcher
+
+Apps = [
+    "Stopwatch",
+    "Timer"
+]
 
 class ClockApp(App):
 
@@ -17,12 +22,23 @@ class ClockApp(App):
 
     def compose(self):
         yield Header(show_clock=True)
+        yield Tabs(
+            Tab(Apps[0], id="stopwatchs"),
+            Tab(Apps[1], id="timers"),
+            active="stopwatchs"
+        )
 
-        with ScrollableContainer(id="stopwatches"):
-            yield Stopwatch()
-            yield Timer()
+        with ContentSwitcher(initial="stopwatchs"):
+                with ScrollableContainer(id="stopwatchs"):
+                    yield Stopwatch()
+
+                with ScrollableContainer(id="timers"):
+                    yield Timer()
 
         yield Footer()
+
+    def on_tabs_tab_activated(self, event: Tabs.TabActivated) -> None:
+        self.query_one(ContentSwitcher).current = event.tab.id
 
     ## Action methods (starts with: action_)
     # Toggle dark mode
