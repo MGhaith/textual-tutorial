7dbca13cd4d0f307b376bbd43a308f9a6f99906d
Ghaith Magherbi <retr0creativegm@gmail.com>
2026-01-04 10:12:54 +0100

    add the progress bar make_progress function

--- clock/widgets/timer.py
+++ clock/widgets/timer.py
@@ -1,9 +1,13 @@
+from time import monotonic
+
 from textual.containers import Center, Middle, VerticalGroup, HorizontalGroup
 from textual.timer import Timer as TextualTimer
 from textual.widgets import Static, ProgressBar, Input, Button
 
 class Timer(Static):
     
+    start_time = monotonic()
+    selected_time = 60
     progress_timer: TextualTimer
     def compose(self):
         with Center():
@@ -20,3 +24,16 @@ class Timer(Static):
     def on_mount(self) -> None:
         # Set up a timer to simulate progess happening.
         self.progress_timer = self.set_interval(1 / 60, self.make_progress, pause=True)
+
+    def make_progress(self) -> None:
+        # Called automatically to advance the progress bar.
+        elapsed = monotonic() - self.start_time
+        print(self.selected_time)
+        remaining_percent = max(0, ((self.selected_time - elapsed) / self.selected_time) * 100)
+        self.query_one(ProgressBar).update(progress=remaining_percent)
+
+        # Stop timer if done
+        if remaining_percent <= 0:
+            self.progress_timer.pause()
+            self.query_one(VerticalGroup).remove_class("hidden")
+            self.query_one(ProgressBar).add_class("hidden")
