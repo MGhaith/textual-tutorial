7f931d3f7f8841a327c88a6ff9b3fe68d49cc6d5
Ghaith Magherbi <retr0creativegm@gmail.com>
2026-01-04 10:05:42 +0100

    finish compose function

--- clock/widgets/timer.py
+++ clock/widgets/timer.py
@@ -1,6 +1,16 @@
-from textual.widgets import Static
+from textual.containers import Center, Middle, VerticalGroup, HorizontalGroup
+from textual.widgets import Static, ProgressBar, Input, Button
 
 class Timer(Static):
     
     def compose(self):
-        yield Static("Timer")
+        yield Static("Timer")        with Center():
+            with Middle():
+                with VerticalGroup():
+                    with HorizontalGroup():
+                        yield Input(value="00", id="hours", type="number")
+                        yield Input(value="01", id="minutes", type="number")
+                        yield Input(value="00", id="seconds", type="number")
+                    yield Button("Start", id="start_timer", variant="success")
+
+                yield ProgressBar(classes="hidden")
